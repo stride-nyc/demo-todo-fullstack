c84382be6d4bbeed3fe247f9b06a58f3ca59cbf2
Josiah Wilkins <lowgen0@gmail.com>
2023-12-21 15:41:19 -0500

    update styles for demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import StrideLogo from "./stride-logo-white.png";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const handleAddTodo = (event) => {
     event.preventDefault();
     setTodos([...todos, input]);
-    setInput('');
+    setInput("");
   };
 
   const handleInputChange = (event) => {
@@ -17,17 +18,36 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleAddTodo}>
-          <input value={input} onChange={handleInputChange} />
-          <button type="submit">Add Todo</button>
-        </form>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
-          ))}
-        </ul>
+      <header className="App-Logo flex justify-center">
+        <img src={StrideLogo} alt="Stride" />
       </header>
+      <p className="TextColor text-5xl">What would you like to do?</p>
+
+      <div className="mx-auto w-1/2">
+        <div>
+          <form onSubmit={handleAddTodo} className="space-y-4">
+            <input
+              value={input}
+              onChange={handleInputChange}
+              className="input input-bordered w-full max-w-xs mx-4"
+            />
+
+            <button type="submit" className="btn mx-4">
+              Add Todo
+            </button>
+          </form>
+        </div>
+
+        <div>
+          <ul className="list-disc list-inside">
+            {todos.map((todo, index) => (
+              <li className="TextColor text-xl text-left mx-auto w-3/5" key={index}>
+                {todo}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
