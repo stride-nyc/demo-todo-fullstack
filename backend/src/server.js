546c0ff40117757636419cc10ef53bb1836ec250
Josiah Wilkins <lowgen0@gmail.com>
2024-01-08 16:49:37 -0500

    use an sqllite db to save data

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,37 @@
 import express from "express";
 import cors from "cors";
+import sqlite3 from "sqlite3";
+import { Database, open } from "sqlite";
 
 const PORT = 3000;
 
 const app = express();
+
+let DB;
+const DATABASE_FILE = "./MY_COOL.db";
+
+async function connect() {
+  if (!DB)
+    DB = await open({
+      filename: DATABASE_FILE,
+      driver: sqlite3.cached.Database,
+    });
+
+  await DB.exec(`
+      CREATE TABLE IF NOT EXISTS TODOs (
+        id INTEGER PRIMARY KEY,
+        description TEXT NOT NULL
+      )
+    `);
+
+  return DB;
+}
+
+async function readAllTodos() {
+  const db = await connect();
+  const results = await db.all("SELECT * FROM TODOs");
+  return results.map((todo) => todo.description);
+}
 
 app.use(
   cors({
@@ -20,17 +48,20 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-const CURRENT_TODOS = ["todo1", "todo2"];
-app.get("/api/todos", (req, res) => {
+app.get("/api/todos", async (req, res) => {
   console.log("GET /api/todos");
-  res.send(CURRENT_TODOS);
+  const current_todos = await readAllTodos();
+  res.send(current_todos);
 });
 
-app.post("/api/todos", (req, res) => {
+app.post("/api/todos", async (req, res) => {
   console.log("POST /api/todos");
   const { todo } = req.body;
-  CURRENT_TODOS.push(todo);
-  res.send(CURRENT_TODOS);
+  const db = await connect();
+  const stmt = await db.prepare("INSERT INTO TODOs (description) VALUES (?)");
+  await stmt.run(todo);
+  const current_todos = await readAllTodos();
+  res.send(current_todos);
 });
 
 app.listen(PORT, () => {
