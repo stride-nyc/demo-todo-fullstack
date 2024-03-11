7f2107144b02d2e2689792b688262807b7c66ed8
Raymond Lam <raymondlam1991@gmail.com>
2024-03-11 14:42:57 -0400

    Run postgres from docker container

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,10 +5,28 @@
  */
 export default {
 
-  development: {
-    client: 'sqlite3',
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './MY_COOL.db'
+  //   }
+  // },
+  
+    development: {
+    client: 'pg',
     connection: {
-      filename: './MY_COOL.db'
+      host : 'localhost',
+      user: 'postgres',
+      password: 'password',
+      database: 'demo_todo_fullstack',
+      port: 54320
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 
