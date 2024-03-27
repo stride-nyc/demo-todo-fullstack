559b61551c36608554a8ae44f18b937a90c96357
Dave A <dave@dvndrsn.com>
2024-03-27 14:00:44 -0400

    use describe with unit test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,21 +1,28 @@
-import { render, fireEvent, screen } from '@testing-library/react';
-import nock from 'nock';
-import App from './App';
+import { render, fireEvent, screen } from "@testing-library/react";
+import nock from "nock";
+import App from "./App";
 
+describe("App", () => {
+  it("creates a new Todo Item when clicking Add Todo", async () => {
+    nock("http://localhost:4000")
+      .get("/api/todos")
+      .reply(200, [], { "Access-Control-Allow-Origin": "*" });
+    nock("http://localhost:4000")
+      .post("/api/todos")
+      .reply(200, ["Test Todo"], { "Access-Control-Allow-Origin": "*" });
 
-test('adds a todo item', async () => {
-  nock('http://localhost:4000').get('/api/todos').reply(200, [], {'Access-Control-Allow-Origin': '*'});
-  nock('http://localhost:4000').post('/api/todos').reply(200, ['Test Todo'], {'Access-Control-Allow-Origin': '*'});
+    render(<App />);
 
-  render(<App />);
-  
-  const inputElement = screen.getByRole('textbox', { name: /todo description/i});
-  const buttonElement = screen.getByRole('button', { name: /add todo/i});
-  
-  fireEvent.change(inputElement, { target: { value: 'Test Todo' } });
-  fireEvent.click(buttonElement);
-  
-  const todoElement = await screen.findByText(/test todo/i);
-  
-  expect(todoElement).toBeInTheDocument();
+    const inputElement = screen.getByRole("textbox", {
+      name: /todo description/i,
+    });
+    const buttonElement = screen.getByRole("button", { name: /add todo/i });
+
+    fireEvent.change(inputElement, { target: { value: "Test Todo" } });
+    fireEvent.click(buttonElement);
+
+    const todoElement = await screen.findByText(/test todo/i);
+
+    expect(todoElement).toBeInTheDocument();
+  });
 });
